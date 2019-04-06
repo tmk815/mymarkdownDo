a0a16ae07a82490bbaf9a661dd0f47628c47c94d
門田朋己 <monndenn15@gmail.com>
2019-04-06 22:26:14 +0900

    https://github.com/nabettu/mymarkdown
    これやった

---
+++ src/views/Terms.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="terms">
+        <h1>利用規約</h1>
+        <p>ここに利用規約が入ります。</p>
+        <router-link :to="{name: 'top'}">TOPに戻る</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "terms"
+};
+</script>
+
